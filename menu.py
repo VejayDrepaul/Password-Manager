2c91814618553db98d5ba3d17bdb07b07fc9b91e
Vejay Drepaul <dvejay@outlook.com>
2021-07-29 14:02:19 -0400

    init commits

---
+++ menu.py
@@ -0,0 +1,11 @@
+def menu():
+    print("------------------------------");
+    print("-------------Menu-------------");
+    print("\n");
+    print("1. Enter an account to be stored in the database");
+    print("2. Find a account information for a site or app");
+    print("3. Change account information in database");
+    print("4. Delete an account from the database");
+    print("5. Generate a password for an account");
+    print("6. Exit");
+    print("\nWhat do you want to do: ");
