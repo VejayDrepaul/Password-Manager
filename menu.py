31f0ab8df5d8bbcd6afef181bac5397213e6428d
Vejay Drepaul <dvejay@outlook.com>
2021-07-31 00:12:13 -0400

    All password manager options

--- menu.py
+++ menu.py
@@ -1,3 +1,7 @@
+import os
+from dotenv import load_dotenv
+from database_manager import *
+
 def menu():
     print("------------------------------");
     print("-------------Menu-------------");
@@ -8,4 +12,23 @@ def menu():
     print("4. Delete an account from the database");
     print("5. Generate a password for an account");
     print("6. Exit");
-    print("\nWhat do you want to do: ");
+    option = int(input("\nWhat do you want to do: "))
+
+    if option == 1:
+        website_name = input("What is the name of the website or app: ")
+        link = input("What is the link to the website or app: ")
+        username = input("What is the username of the account: ")
+        email = input("What is the email used for the account: ")
+        password = input("What is the password for your account: ")
+
+        store_new_account(website_name, link, username, email, password)
+
+
+def login():
+    load_dotenv()
+    MASTER_PASSWORD = os.getenv('MASTER_PASSWORD')
+
+    password = input("Please provide the master password: ")
+
+    if password == MASTER_PASSWORD:
+        menu()
