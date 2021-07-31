c50aef6a21306e31281b2a5e718510dd90c78724
Vejay Drepaul <dvejay@outlook.com>
2021-07-31 00:12:06 -0400

    Main file

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-def main();
-    
+from menu import *
+from database_manager import *
+
+
+def main():
+    login()
+    #connection()
+    #table_creation()
+
+
+if __name__ == "__main__":
+    main()
