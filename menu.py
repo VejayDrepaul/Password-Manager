dcdbb2ff57a919c6429f39ed65767003c1f893c7
Vejay Drepaul <dvejay@outlook.com>
2021-08-02 09:36:22 -0400

    Menu file

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 import os
+import getpass
 from dotenv import load_dotenv
 from database_manager import *
 
@@ -6,32 +7,41 @@ def menu():
     print("------------------------------");
     print("-------------Menu-------------");
     print("\n");
-    print("1. Enter an account to be stored in the database");
-    print("2. Find account information for a specific site or app");
+    print("1. Enter an account to be stored in the database")
+    print("2. Find account information for a specific site or app")
     print("3. List all accounts")
-    print("4. Change account information in database");
-    print("5. Delete an account from the database");
-    print("6. Generate a password for an account");
-    print("7. Exit");
+    print("4. Change account information in database")
+    print("5. Delete an account from the database")
+    print("6. Generate a password for an account")
+    print("7. Export account information as csv file")
+    print("8. Exit");
     option = int(input("\nWhat do you want to do: "))
+
 
     if option == 1:
         website_name = input("What is the name of the website or app: ")
-        link = input("What is the link to the website or app: ")
+        link = input("What is the link to the website (if applicable): ")
         username = input("What is the username of the account: ")
         email = input("What is the email used for the account: ")
         password = input("What is the password for your account: ")
-
         store_new_account(website_name, link, username, email, password)
+    elif option == 2:
+        find_accounts()
     elif option == 3:
         list_accounts()
+    elif option == 7:
+        file_type = input("Do you want the passwords in a (c)sv or (e)xcel file: ")
+        if file_type == 'c':
+            csv()
+        elif file_type == 'e':
+            xlsx()        
 
 
 def login():
     load_dotenv()
     MASTER_PASSWORD = os.getenv('MASTER_PASSWORD')
 
-    password = input("Please provide the master password: ")
+    password = getpass.getpass("Please provide the master password: ")
 
     if password == MASTER_PASSWORD:
         menu()
