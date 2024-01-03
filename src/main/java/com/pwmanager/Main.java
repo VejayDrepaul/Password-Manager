a59940acdcd75e94b0406e07af4355a2c0e5ffbe
Vejay Drepaul <vejayddrepaul@gmail.com>
2024-01-02 23:19:08 -0500

    init commits

---
+++ src/main/java/com/pwmanager/Main.java
@@ -0,0 +1,14 @@
+package com.pwmanager;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+        frame.setTitle("Password Manager");
+
+        
+    }
+}
