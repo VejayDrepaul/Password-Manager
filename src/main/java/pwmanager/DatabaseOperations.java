dfa6a4927bcf4482b69fc933c75ba3d306355ca2
Vejay Drepaul <vejayddrepaul@gmail.com>
2024-01-03 22:14:17 -0500

    All database operations working

--- src/main/java/pwmanager/DatabaseOperations.java
+++ src/main/java/pwmanager/DatabaseOperations.java
@@ -3,7 +3,11 @@ package pwmanager;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import javax.naming.spi.DirStateFactory.Result;
+import javax.swing.table.DefaultTableModel;
 
 public class DatabaseOperations {
     public static DatabaseOperations instance = null;
@@ -33,14 +37,15 @@ public class DatabaseOperations {
         return false;
     }
 
-    public boolean addAccounts(String account, String username, String password, String accountNotes) {
-        String insertSQL = "INSERT INTO accounts (account, username, password, account_notes) VALUES (?, ?, ?, ?)";
+    public boolean addAccounts(String account, String username, String password, String accountNotes, String hash) {
+        String insertSQL = "INSERT INTO accounts (account, username, password, account_notes, hashed_password) VALUES (?, ?, ?, ?, ?)";
 
         try (PreparedStatement prepStatement = dbConn.prepareStatement(insertSQL)) {
             prepStatement.setString(1, account);
             prepStatement.setString(2, username);
             prepStatement.setString(3, password);
             prepStatement.setString(4, accountNotes);
+            prepStatement.setString(5, hash);
 
             int rowsAffected = prepStatement.executeUpdate();
 
@@ -69,6 +74,28 @@ public class DatabaseOperations {
             } else {
                 System.out.println("Account not found or failed to remove.");
             }
+        } catch (SQLException e) {
+            System.err.println("SQL Exception: " + e.getMessage());
+        }
+
+        return false;
+    }
+
+    public boolean populateTable(DefaultTableModel table) {
+        String sqlQuery = "SELECT account, username, password, account_notes FROM accounts";
+
+        try (PreparedStatement statement = dbConn.prepareStatement(sqlQuery)) {
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                String account = result.getString("account");
+                String username = result.getString("username");
+                String password = result.getString("password");
+                String account_notes = result.getString("account_notes");
+                table.addRow(new Object[]{account, username, password, account_notes});
+            }
+
+            return true;
         } catch (SQLException e) {
             System.err.println("SQL Exception: " + e.getMessage());
         }
